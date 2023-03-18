43dfb05e34c96c59fc22dedc869a9600decfbe12
Fury-AF <99542261+Fury-AF@users.noreply.github.com>
2023-03-18 09:41:47 +0100

    java2

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-window.addEventListener("load" ,init )
+window.addEventListener("load", init);
 
-function init(){
+const kutyaLista = [
+    "foxterrier",
+    "kuvasz",
+    "puli",
+    "agár",
+    "újfullandi",
+    "keverék",
+  ];
 
 
-const kutyaLista = ["foxterrier", "kuvasz", "puli", "agár", "újfullandi", "keverék"];
+function init() {
 
-
-const article = document.getElementById("kutya-article");
+  /*const article = document.getElementById("kutya-article");
 const button = document.createElement("button");
 button.innerText = "Kutya";
 article.appendChild(button);
@@ -15,23 +21,28 @@ article.appendChild(button);
 button.addEventListener("click", function() {
  
   article.innerText = kutyaLista[2];
-});
+});*/
 
+  const ARTICLE = document.querySelectorAll("kutya-article");
+
+  let txt = osszeallit();
+
+  // itt helyezzük bele az article elembe
+  ARTICLE[0].innerHTML = txt;
 }
 
-function osszeallit(){
-    //összeállítjuk azt a szöveget tartalmat, ami a HTML kódot jelenti 
-let i=0;
-for (let index = 0; index < kutyaLista.length; index++) {
-    txt+=` 
+function osszeallit() {
+  //összeállítjuk azt a szöveget tartalmat, ami a HTML kódot jelenti
+  let txt = "";
+  for (let index = 0; index < kutyaLista.length; index++) {
+    txt += ` 
 <div>
     <p>
     ${kutyaLista[index]}
     </p>
     
-</div>`
-    
-}
-console.log(txt);
-return txt
+</div>`;
+  }
+  console.log(txt);
+  return txt;
 }
